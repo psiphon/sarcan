be8e7cfdbc2f81f6c09ac841585dddaaba2010a7
William Michaels <bill@polarity.io>
2024-02-04 23:35:59 -0600

    feat: run tts local

--- app/personalities/glados.py
+++ app/personalities/glados.py
@@ -1,5 +1,11 @@
 import requests
 import os
+import torch
+import torch
+from utils.tools import prepare_text
+from scipy.io.wavfile import write
+import time
+from sys import modules as mod
 import urllib.parse
 from config import Config
 import hashlib
@@ -7,6 +13,29 @@ import hashlib
 class Glados:
     def __init__(self, config):
         self.config = config
+        print("Initializing TTS Engine...")
+
+        # Select the device
+        if torch.is_vulkan_available():
+            device = 'vulkan'
+        if torch.cuda.is_available():
+            device = 'cuda'
+        else:
+            device = 'cpu'
+
+        # Load models
+        glados = torch.jit.load('app/models/glados.pt')
+        vocoder = torch.jit.load('app/models/vocoder-gpu.pt', map_location=device)
+
+        # Prepare models in RAM
+        for i in range(4):
+            init = glados.generate_jit(prepare_text(str(i)))
+            init_mel = init['mel_post'].to(device)
+            init_vo = vocoder(init_mel)
+        
+        self.vocoder = vocoder
+        self.glados = glados
+        self.device = device
 
     def get_chatgpt_instruction(self):
         return "responses should be in the style of glados from portal game"
@@ -17,26 +46,36 @@ class Glados:
         return md5_hash.hexdigest()
     
     def text_to_voice(self, text):
-        # Encode the answer
-        encoded_answer = urllib.parse.quote(text)
+        # Tokenize, clean and phonemize input text
+        x = prepare_text(text).to('cpu')
 
-        # Send the encoded answer as a query param to localhost:1234/synthesize/ and save the resulting binary response to /tmp/output.wav
-        url = self.config.get_glados_server() + f"/synthesize/?text={encoded_answer}"
-        r = requests.get(url)
+        with torch.no_grad():
 
-        # if the response is not 200 then throw an error
-        if r.status_code != 200:
-            raise Exception("Error from Glados TTS server. Status code: " + str(r.status_code))
+            # Generate generic TTS-output
+            old_time = time.time()
+            tts_output = self.glados.generate_jit(x)
+            print("Forward Tacotron took " + str((time.time() - old_time) * 1000) + "ms")
 
-        # md5 hash the text to get a unique filename
+            # Use HiFiGAN as vocoder to make output sound like GLaDOS
+            old_time = time.time()
+            mel = tts_output['mel_post'].to(self.device)
+            audio = self.vocoder(mel)
+            print("HiFiGAN took " + str((time.time() - old_time) * 1000) + "ms")
+            
+            # Normalize audio to fit in wav-file
+            audio = audio.squeeze()
+            audio = audio * 32768.0
+            audio = audio.cpu().numpy().astype('int16')
         
-        md5 = self.calculate_md5(text)
-        filename = f"{md5}.wav"
-        tmpfile = os.path.join('app',self.config.get_audio_dir(), filename)
-        with open(tmpfile, 'wb') as f:
-            f.write(r.content)
+            md5 = self.calculate_md5(text)
+            filename = f"{md5}.wav"
+            tmpfile = os.path.join('app',self.config.get_audio_dir(), filename)
 
-        return '/audio/'+filename
+            # Write audio file to disk
+            # 22,05 kHz sample rate
+            write(tmpfile, 22050, audio)
+
+            return '/audio/'+filename
     
     def get_error_message(self):
         return {
